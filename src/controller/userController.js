409506f5c03d6e81833d144c9e97011dd5b9b4eb
TranDuc <tranthaitrongduc@gmail.com>
2025-12-30 22:14:28 +0700

    create /allcode for user account"

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -62,10 +62,23 @@ let handleDeleteUser = async (req, res) => {
     let message = await userService.deleteUser(req.body.id);
     return res.status(200).json(message);
 }
+let getAllCode = async (req, res) => {
+    try {
+        let data = await userService.getAllCodeService(req.query.type);
+        return res.status(200).json(data);
+    } catch (e) {
+        console.log('Get allcode error: ', e);
+        return res.status(200).json({
+            errCode: -1,
+            message: "Error from server",
+        })
+    }
+}
 module.exports = {
     handleLogin: handleLogin,
     handleGetAllUser: handleGetAllUser,
     handleCreateNewUser: handleCreateNewUser,
     handleEditUser: handleEditUser,
-    handleDeleteUser: handleDeleteUser
+    handleDeleteUser: handleDeleteUser,
+    getAllCode: getAllCode
 }
--- src/service/userService.js
+++ src/service/userService.js
@@ -211,11 +211,33 @@ let updateUserData = (data) => {
         }
     })
 }
+let getAllCodeService = (typeInput) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!typeInput) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing required parameters"
+                })
+            } else {
+                let res = {};
+                let allcode = await db.AllCodes.findAll({
+                    where: { type: typeInput }
+                });
+                res.errCode = 0;
+                res.data = allcode;
+                resolve(res);
+            };
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
 module.exports = {
     handleUserLogin: handleUserLogin,
     getAllUser: getAllUser,
     createNewUser: createNewUser,
     deleteUser: deleteUser,
     updateUserData: updateUserData,
-    // compareUserPassword: compareUserPassword
+    getAllCodeService: getAllCodeService
 }
